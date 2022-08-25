63388533ab3c8991cdd9882a554fd47e86b38983
Oli Japa <olialexander1131@gmail.com>
2022-08-24 22:01:22 -0400

    oli changes

---
+++ Katas.Range.Tests/kara-range-test.cs
@@ -0,0 +1,20 @@
+namespace Katas.Range.Tests;
+
+public class KataRangeTest
+{
+    [Test]
+    public void should_throw_an_error_if_contains_letters()
+    {
+
+        Assert.That(() => new Range("(2,abc]"), Throws.ArgumentException);
+    }
+
+    [Test]
+    public void range_input_should_be_equal_to_the_expected_value()
+    {
+
+        Assert.That(() => new Range("(2,6]"), Throws.ArgumentException);
+    }
+
+
+}
