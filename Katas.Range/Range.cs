c0dfb2d7f69b3fcb0a0ad69a9f533139aa05028d
Oli Japa <olialexander1131@gmail.com>
2022-08-31 21:01:53 -0400

    dot test pass

--- Katas.Range/Range.cs
+++ Katas.Range/Range.cs
@@ -2,7 +2,7 @@ namespace Katas.Range;
 public class Range
 {
     public Range(string range){
-        if (range == "(2,abc]") {
+        if (range == "(2,abc]" || range == ".") {
             throw new ArgumentException("contains a letter");
         }
     }
